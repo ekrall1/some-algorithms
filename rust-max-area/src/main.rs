daed4598724f484983115da0932b9faf5bcc5db6
Eric Krall <krall.eric@gmail.com>
2024-08-20 04:22:07 -0400

    max container area along an axis

--- rust-max-area/src/main.rs
+++ rust-max-area/src/main.rs
@@ -1,5 +1,6 @@
 // find container that holds max area between any two lines along an axis
 use std::cmp;
+
 pub struct Containers {
     heights: Vec<i32>,
 }
@@ -9,17 +10,18 @@ impl Containers {
         Self { heights: h }
     }
 
+    fn _area(&self, left_idx: usize, right_idx: usize) -> i32 {
+        (right_idx as i32 - left_idx as i32)
+            * cmp::min(self.heights[left_idx], self.heights[right_idx])
+    }
+
     pub fn find_max_area(self) -> i32 {
         let mut max_area: i32 = 0;
         let mut left_idx: usize = 0;
         let mut right_idx: usize = self.heights.len() - 1;
 
         while left_idx < right_idx {
-            max_area = cmp::max(
-                max_area,
-                (right_idx as i32 - left_idx as i32)
-                    * cmp::min(self.heights[left_idx], self.heights[right_idx]),
-            );
+            max_area = cmp::max(max_area, self._area(left_idx, right_idx));
 
             match self.heights[left_idx] < self.heights[right_idx] {
                 true => left_idx += 1,
